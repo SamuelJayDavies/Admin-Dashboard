bc6a6acc09e5c18590589b8c9df81630c55f6c1a
Samuel Davies <samj.davies@virginmedia.com>
2024-06-05 15:18:41 +0100

    Began creating skeleton of the website

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const firstNames = [
     "Aaron", "Abigail", "Adrian", "Aiden", "Alexa", "Alexander", "Alice", "Alyssa", "Amanda", "Amber",
     "Andrew", "Angela", "Anna", "Anthony", "Ashley", "Austin", "Ava", "Benjamin", "Blake", "Brandon",
     "Brianna", "Brian", "Brooke", "Caleb", "Cameron", "Carlos", "Catherine", "Charlotte", "Chloe", "Christian",
-    "Christopher", "Claire", "Daniel", "David", "Dominic", "Dylan", "Elijah", "Elizabeth", "Emily", "Emma",
+    "Christopher", "Claire", "Danny", "David", "Dominic", "Dylan", "Elijah", "Elizabeth", "Emily", "Emma",
     "Ethan", "Evelyn", "Gabriel", "Grace", "Hannah", "Isaac", "Isabella", "Jacob", "James", "Jasmine"
 ];
 
+const lastNames = [
+    "Anderson", "Bailey", "Baker", "Barnes", "Bell", "Bennett", "Brooks", "Brown", "Butler", "Campbell",
+    "Carter", "Clark", "Collins", "Cook", "Cooper", "Cox", "Davis", "Diaz", "Edwards", "Evans",
+    "Fisher", "Foster", "Garcia", "Gomez", "Gonzalez", "Gray", "Green", "Hall", "Harris", "Hernandez",
+    "Hill", "Howard", "Hughes", "Jackson", "James", "Jenkins", "Johnson", "Jones", "Kelly", "King",
+    "Lee", "Lewwy", "Long", "Lopez", "Martin", "Martinez", "Miller", "Moore", "Morgan", "Morris"
+];
+
+function generateRandomName() {
+    const randomIndex = Math.floor(Math.random() * firstNames.length);
+    const firstName = firstNames[randomIndex];
+    const lastName = lastNames[randomIndex];
+    const randomName = firstName + ' ' + lastName;
+    document.getElementById('random-name').textContent = randomName;
+    document.getElementById('user-details-name').textContent = randomName + ' (@' 
+        + firstName.toLowerCase() + lastName.substring(0,1) + ')';
+}
+
+window.onload = generateRandomName;
